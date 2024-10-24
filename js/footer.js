bc7c76314a5d1c881a9a980e50236ef944f2161e
Cofallen <1727334304@qq.com>
2024-10-24 12:56:34 +0900

    Site updated: 2024-10-24 12:56:34

--- js/footer.js
+++ js/footer.js
@@ -24,7 +24,7 @@
       }
       document.getElementById("timeDate").innerHTML = "🚀已持续航行&nbsp"+dnum+"&nbsp天";  
       document.getElementById("times").innerHTML = hnum + "&nbsp时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
-      document.getElementById("customMessage").innerHTML = "<br>人生而自由，却无时不刻处于枷锁之中。";
+      document.getElementById("customMessage").innerHTML = "<br>我很喜欢你，但你是自由的";
     }
   
     update();
